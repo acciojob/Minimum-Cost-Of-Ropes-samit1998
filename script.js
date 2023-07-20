be1ed2148b45064e6e476c4bdd0d22738026b8e0
SAMIT KUMAR BARIK <126311037+samit1998@users.noreply.github.com>
2023-07-20 16:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ function compare(a, b) {
   }
 }
  
- 
- 
 function calculateMinCost() {
   //your code here
   let str=document.getElementById('rope-lengths').value.split(",");
@@ -39,37 +37,4 @@ console.log(total);
  
 let result=document.getElementById('result');
 result.innerHTML=total;
-}  
-
-
-function calculateMinCost() {
-  //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
-  }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
-  
 }  
